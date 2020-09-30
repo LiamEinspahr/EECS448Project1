6a1d88ad9f8825b3e3198d918b21879ba5e970c9
Kayton Froeschl <kfroeschl@gmail.com>
2020-09-29 20:27:28 -0500

    med.cpp

--- medium.cpp
+++ medium.cpp
@@ -3,21 +3,48 @@
 #include "board.h"
 #include "medium.h"
 
-int Medium:: randomNum(){
-    //add more conditions if they choose the xl board
-    return((rand() % 9) + 1);
+Medium::Medium(){
+    row = 0;
+    col = 0;
 }
 
-char Medium:: randomChar(){
+int Medium::charToInt(char c) {return ((toupper(c) - 65));}
+
+int Medium:: randomNum(){
+    //add more conditions if they choose the xl board
+    row = (rand() % 9) + 1;
+    return(row);
+}
+
+int Medium:: randomChar(){
     char c;
     int r;
+    int key;
 
     srand (time(NULL));    
     r = rand() % 9;   
-    c = 'a' + r;           
-    return(c);
+    c = 'a' + r;
+    key = charToInt(c);
+    col = key;
+    return(col);
 }
 
-bool Medium:: isValidMove(int row, int col){
+int Medium::getRow(){
+    return(row);
+}
+
+int Medium:: getCol(){
+    return(col);
+}
+
+void Medium::solve(){
+    
+}
+
+bool Medium::move(int row, int col){
+
+}
+
+bool Medium:: isValidMove(int row, char col){
 
 }
