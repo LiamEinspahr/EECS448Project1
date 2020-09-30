c2a42000b5ded305e0158917cb1dbf0336fc87cf
Celia Babst <celiababst@gmail.com>
2020-09-29 22:47:54 -0500

    board.cpp
    Still not done but began working on XL board

--- board.cpp
+++ board.cpp
@@ -27,8 +27,10 @@ Board:: Board(bool big)
 
 Board::~Board() {}
 
-void Board::printBoard()
+void Board::printBoard(bool big)
 {
+	if(big == false)
+	{
 	int sideNum = 1;
 	for(int i=0; i<numRows; i++)
 	{
@@ -52,7 +54,36 @@ void Board::printBoard()
 		sideNum++;
 		cout << "\n";
 	}
+	}
+	if(big == true)
+	{
+	int sideNum = 1;
+	for(int i=0; i<numRows; i++)
+	{
+		for(int j=0; j<numCols; j++)
+		{
+			if(i == 0 && j == 0)
+			{
+				cout << "  ";
+				for(char c = 'A'; c <= 'S'; c++)
+				{
+					cout << c << " ";
+				}
+				cout << "\n";
+			}
+			if(j == 0)
+			{
+				cout << sideNum << " ";
+			}
+			cout << m_board[i][j] << " ";
+		}
+		sideNum++;
+		cout << "\n";
+	}
+	}
 }
+
+
 
 void Board::updateBoard(int row, int col, char c)
 {
