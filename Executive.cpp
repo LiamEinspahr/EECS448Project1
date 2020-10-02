1174c50d576e049807fd3356f10def9126b5690d
Amanda Nelson <anelson99@ku.edu>
2020-10-02 15:21:21 -0500

    Another Bug fix

--- Executive.cpp
+++ Executive.cpp
@@ -253,7 +253,7 @@ void Executive::run()
     if (!humanOpponent) {
         //blank Board
         display.friendlyBoard(currentPlayer->my_ships);
-        char direction = 'u'; //default direction is up
+        char direction = 'U'; //default direction is up
 
 
         cin.ignore();
